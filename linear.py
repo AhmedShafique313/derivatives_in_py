f766a75c030516ca06954fc72899f71bf36269c7
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-01-26 09:44:51 +0500

    got derivative of function f(a)

--- linear.py
+++ linear.py
@@ -1,2 +1,17 @@
-print("Hello world")
-print("Lets Start")
+# Lets start our code of derivative in python 
+
+# lets consider we have function f(a) = 3a
+# if we take derivative of f(a) then we get d(f(a))/da = d(3a)/da = 3.1= 3
+
+# derivative 
+a = input("Enter the value of a: ")
+a= float(a)
+# checking order of function 
+cons = 3
+func = cons * a
+diff = a / a
+derivative = cons*diff
+print("Derivative of f(a) = ", derivative)
+
+# lets plot f(a) = 3a
+
